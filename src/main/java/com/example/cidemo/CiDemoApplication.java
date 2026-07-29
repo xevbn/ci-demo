dd5472837e320a1e0d467722737629e684b13f98
test <hello.git.GitHub@lomeafoer@gmail.com>
2026-07-29 17:34:58 +0900

    Initial commit

---
+++ src/main/java/com/example/cidemo/CiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiDemoApplication.class, args);
+    }
+
+}
